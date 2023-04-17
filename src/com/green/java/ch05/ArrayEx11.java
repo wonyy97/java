afdf78ba1b2657fb111f2ce43e1f97d02b070cc9
Peanuts <kwh1619@naver.com>
2023-04-17 19:59:16 +0900

    이해가 잘 안되노..

---
+++ src/com/green/java/ch05/ArrayEx11.java
@@ -0,0 +1,22 @@
+package com.green.java.ch05;
+
+public class ArrayEx11 {
+    public static void main(String[] args) {
+        int numArr[] = new int[10];
+        int conter[] = new int[10];
+
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = (int) (Math.random() * 10);
+            System.out.print(numArr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < numArr.length; i++) {
+            conter[numArr[i]]++;
+        }
+
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.println(i+ "의 개수 " + conter[i]);
+        }
+    }
+}
