9e038758de4f312ff257d72c1c530c9bbf8aa369
Administrator <smk93021@gmai.com>
2023-04-18 19:19:20 +0900

    다시보기

--- src/com/green/java/ch05/ArrayEx19_1.java
+++ src/com/green/java/ch05/ArrayEx19_1.java
@@ -1,53 +1,58 @@
 package com.green.java.ch05;
+
+/*
+    번호  국어  영어  수학  총점  평균
+    ==============================
+    1   100    90    80  270   90.0
+    2    20    20    20   60   20.0
+    ...
+    ==============================
+    총점
+    국어: 190
+    영어: 180
+    수학: 170
+
+ */
 public class ArrayEx19_1 {
     public static void main(String[] args) {
+        String[] titles = { "국어", "영어", "수학", "사회" };
+        int[] titleSumArr = new int[titles.length];
         int[][] score = {
-                {100, 90, 80}
-                , {20, 20, 20}
-                , {30, 30, 30}
-                , {40, 40, 40}
+                { 100, 90, 80, 10 }
+                , {  20, 20, 20, 20 }
+                , {  30, 30, 30, 30 }
+                , {  40, 40, 40, 40 }
         };
-    
-        int sum[] = new int[score.length];
-        double avg[] = new double[score.length];
-        int total[] =  new int[score[0].length];
-
-        System.out.println("번호\t\t국어\t\t영어\t\t수학\t\t총점\t\t평균");
-        System.out.println("============================================");
-
-
-        for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < score[0].length; j++) {
-                sum[i] += score[i][j];
+        System.out.print("번호\t");
+        for(int i=0; i<titles.length; i++) {
+            System.out.printf("%s\t", titles[i]);
+        }
+        System.out.println("총점\t평균");
+        System.out.println("==============================");
+        for(int i=0; i<score.length; i++) {
+            int[] eachScore = score[i];
+            int sum = 0;
+            System.out.printf("%3d\t", i + 1);
+            for(int z=0; z<eachScore.length; z++) {
+                int jumsu = eachScore[z];
+                //int jumsu = score[i][z];
+                System.out.printf("%3d\t", jumsu);
+                sum += jumsu;
+                titleSumArr[z] += jumsu;
             }
+            System.out.printf("%3d\t%4.1f\n", sum, ((double)sum / eachScore.length));
         }
-
-        for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < score[i].length; j++) {
-                avg[i] = sum[i] / score[0].length;
-            }
-        }
-
-
-        for (int i = 0; i < score.length; i++) {
-
-            System.out.printf("%d\t\t%3d\t\t%3d\t\t%3d\t\t%3d\t\t%.1f\n",
-                    i + 1, score[i][0], score[i][1], score[i][2], sum[i], avg[i]);
-        }
-
-
-        for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < score[0].length; j++) {
-                total[j] += score[i][j];
-            }
-        }
-
-
-        System.out.println("============================================");
+        System.out.println("==============================");
         System.out.println("총점");
-        System.out.println("국어: " + total[0]);
-        System.out.println("영어: " + total[1]);
-        System.out.println("수학: " + total[2]);
+        for(int i=0; i<titles.length; i++) {
+            System.out.printf("%s: %d\n", titles[i], titleSumArr[i]);
+        }
+
+
+        String str = "ab2d43";
+        char[] charArr = str.toCharArray();
+        System.out.printf("%c", charArr[1]);
+
+
     }
 }
-
---
+++ src/com/green/java/ch05/ArrayEx19_3.java
@@ -0,0 +1,51 @@
+package com.green.java.ch05;
+public class ArrayEx19_3 {
+    /*
+    번호  국어  영어  수학  총점  평균
+    ==============================
+    1   100    90    80  270   90.0
+    2    20    20    20   60   20.0
+    ...
+    ==============================
+    총점
+    국어: 190
+    영어: 180
+    수학: 170
+
+     */
+    public static void main(String[] args) {
+        String[] titles = { "국어", "영어", "수학"};
+        int[] titleSumArr = new int[titles.length];
+        int[][] score = {
+                {100, 90, 80}
+                , {20, 20, 20}
+                , {30, 30, 30}
+                , {40, 40, 40}
+        };
+        System.out.print("번호\t");
+        for(int i = 0; i < titles.length; i++) {
+            System.out.printf("%s\t", titles[i]);
+        }
+
+        System.out.println("총점\t 평균");
+        System.out.println("===============================");
+        for (int i = 0; i < score.length; i++) {
+            int[] eachscore = score[i];
+            int sum = 0;
+            System.out.printf("%3d\t",i+1);
+            for (int j = 0; j < eachscore.length; j++) {
+                int jumsu = eachscore[j];
+                //int jumsu = score[i][j];
+                System.out.printf("%3d\t", jumsu);
+                sum += jumsu;
+                titleSumArr[j] += jumsu;
+            }
+            System.out.printf("%3d\t%4.1f\n", sum, ((double)sum / eachscore.length));
+        }
+        System.out.println("=================================");
+        System.out.println("총점");
+        for (int i = 0; i < titles.length; i++) {
+            System.out.printf("%s: %d\n", titles[i], titleSumArr[i]);
+        }
+    }
+}
