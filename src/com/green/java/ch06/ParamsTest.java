0754f18205b24e1e0f0ac46d081cbc63eaa9c0c2
wonyy97 <kwh1619@naver.com>
2023-04-20 18:32:04 +0900

    Method2

---
+++ src/com/green/java/ch06/ParamsTest.java
@@ -0,0 +1,29 @@
+package com.green.java.ch06;
+//p.265 ~ 267
+public class ParamsTest {
+    public static void main(String[] args) {
+        int num = 10;
+        changeNum(num);
+        System.out.println(num);
+
+        NumberBox nb = new NumberBox();
+        nb.num = 10;
+        changeNum2(nb);
+        System.out.println(nb.num);
+
+    }
+
+    static void changeNum(int num) {
+        num =20;
+    }
+
+    static void changeNum2(NumberBox nb2) {
+        nb2.num = 20;
+    }
+
+}
+
+
+class NumberBox {
+    int num;
+}
