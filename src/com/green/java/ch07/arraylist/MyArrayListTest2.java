925b9adca5dbf31058565da52f347e09f15e0b9a
wonyy97 <kwh1619@naver.com>
2023-04-27 17:45:39 +0900

    04.27 ArrayList

---
+++ src/com/green/java/ch07/arraylist/MyArrayListTest2.java
@@ -0,0 +1,15 @@
+package com.green.java.ch07.arraylist;
+
+public class MyArrayListTest2 {
+    public static void main(String[] args) {
+        MyArrayList myList = new MyArrayList();
+        myList.add(10);
+        myList.add(22);
+        myList.add(4);
+        myList.add(200);
+        myList.add(53);
+        myList.add(96);
+
+        myList.bubbleSort();
+    }
+}
