9d469e83f36f56d3db3b7778070f93d3adad293a
wonyy97 <kwh1619@naver.com>
2023-05-24 16:08:36 +0900

    05.24 빌더

---
+++ src/com/green/java/builder/CarTest.java
@@ -0,0 +1,22 @@
+package com.green.java.builder;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = Car.builder()
+                .cc(1000)
+                .build();
+
+        System.out.println(c1);
+
+        Car c2 = Car.builder()
+                .model("소나타")
+                .seatCnt(5)
+                .build();
+        System.out.println(c2);
+
+
+
+
+
+    }
+}
